2f24414b95748ecbc8da1ad2a403315452dca788
Georgy Shopov <gsshopov@gmail.com>
2015-09-17 11:52:44 -0400

    codeforces; solve A. "Raising Bacteria" from Round #320 (Div. 2)

---
+++ codeforces/round-320/div-2/README.md
@@ -0,0 +1,5 @@
+# Round #320 (Div. 2)
+
+* [A. Raising Bacteria][]
+
+[A. Raising Bacteria]: http://codeforces.com/contest/579/problem/A
---
+++ codeforces/round-320/div-2/raising_bacteria.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+inline
+void use_io_optimizations()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
+
+int main()
+{
+    use_io_optimizations();
+
+    unsigned int bacteria;
+    cin >> bacteria;
+
+    cout << __builtin_popcount(bacteria) << '\n';
+
+    return 0;
+}
