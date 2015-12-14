f548fe394df22a7823491861d941c60dafd555ae
Georgi Shopov <gsshopov@gmail.com>
2015-12-14 12:42:17 -0500

    codeforces; update "Spongebob and Joke" from Round #332 (Div. 2)
    I'm trying to stay away from very long functions, classes an state
    modification.

--- codeforces/round-332/div-2/spongebob_and_joke.cpp
+++ codeforces/round-332/div-2/spongebob_and_joke.cpp
@@ -10,54 +10,89 @@ void use_io_optimizations()
     cin.tie(nullptr);
 }
 
+vector<unsigned int> read_sequence(unsigned int length)
+{
+    vector<unsigned int> sequence(length);
+
+    for (auto& element : sequence)
+    {
+        cin >> element;
+    }
+
+    return sequence;
+}
+
+vector<vector<unsigned int>> inverse(const vector<unsigned int>& sequence)
+{
+    vector<vector<unsigned int>> indices(sequence.size() + 1);
+
+    for (vector<unsigned int>::size_type i {0}; i < sequence.size(); ++i)
+    {
+        indices[sequence[i]].push_back(i + 1);
+    }
+
+    return indices;
+}
+
+bool is_impossible(const vector<unsigned int>& sequence,
+                   const vector<vector<unsigned int>>& indices)
+{
+    for (auto element : sequence)
+    {
+        if (indices[element].empty())
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+bool is_ambiguous(const vector<unsigned int>& sequence,
+                  const vector<vector<unsigned int>>& indices)
+{
+    for (auto element : sequence)
+    {
+        if (indices[element].size() > 1)
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+vector<unsigned int> initial_sequence(const vector<unsigned int>& sequence,
+                                      const vector<vector<unsigned int>>& indices)
+{
+    vector<unsigned int> initial(sequence.size());
+
+    for (vector<unsigned int>::size_type i {0}; i < sequence.size(); ++i)
+    {
+        initial[i] = indices[sequence[i]].front();
+    }
+
+    return initial;
+}
+
 int main()
 {
     use_io_optimizations();
 
-    unsigned int length;
-    unsigned int upper_limit;
+    unsigned int f_length;
+    unsigned int b_length;
 
-    cin >> length >> upper_limit;
+    cin >> f_length >> b_length;
 
-    vector<vector<unsigned int>> indices(length + 1);
+    auto sequence_f = read_sequence(f_length);
+    auto sequence_b = read_sequence(b_length);
+    auto indices    = inverse(sequence_f);
 
-    for (unsigned int i {0}; i < length; ++i)
-    {
-        unsigned int element;
-        cin >> element;
-
-        indices[element].push_back(i);
-    }
-
-    bool is_impossible {false};
-    bool is_ambiguous  {false};
-
-    vector<unsigned int> initial(upper_limit);
-
-    for (unsigned int i {0}; i < upper_limit; ++i)
-    {
-        unsigned int element;
-        cin >> element;
-
-        if (indices[element].empty())
-        {
-            is_impossible = true;
-        }
-        else if (indices[element].size() > 1)
-        {
-            is_ambiguous = true;
-        }
-        else
-        {
-            initial[i] = indices[element].front();
-        }
-    }
-
-    if (is_impossible)
+    if (is_impossible(sequence_b, indices))
     {
         cout << "Impossible\n";
     }
-    else if (is_ambiguous)
+    else if (is_ambiguous(sequence_b, indices))
     {
         cout << "Ambiguity\n";
     }
@@ -65,9 +100,11 @@ int main()
     {
         cout << "Possible\n";
 
-        for (unsigned int i {0}; i < upper_limit; ++i)
+        auto sequence_a = initial_sequence(sequence_b, indices);
+
+        for (auto i = sequence_a.cbegin(); i != sequence_a.cend(); ++i)
         {
-            cout << initial[i] + 1 << " \n"[i + 1 == upper_limit];
+            cout << *i << " \n"[i + 1 == sequence_a.cend()];
         }
     }
 
