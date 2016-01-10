5b94b1b6268b4f3072c52321b3f40736dbde03fe
Georgi Shopov <gsshopov@gmail.com>
2016-01-10 15:22:18 -0500

    uri; solve 1329 "Head or Tail"

---
+++ uri/ad-hoc/head_or_tail.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+inline
+void use_io_optimizations()
+{
+    ios_base::sync_with_stdio();
+    cin.tie(nullptr);
+}
+
+int main()
+{
+    use_io_optimizations();
+
+    for (unsigned int games; cin >> games && games > 0; )
+    {
+        unsigned int wins_mary {0};
+        unsigned int wins_john {0};
+
+        for (unsigned int i {0}; i < games; ++i)
+        {
+            bool joh_won;
+            cin >> joh_won;
+
+            if (joh_won)
+            {
+                ++wins_john;
+            }
+            else
+            {
+                ++wins_mary;
+            }
+        }
+
+        cout << "Mary won " << wins_mary << " times and "
+             << "John won " << wins_john << " times\n";
+    }
+
+    return 0;
+}
