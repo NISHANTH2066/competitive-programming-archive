03f35ff47e7b168e8a9a13507d7c3bb294f1cac5
Georgi Shopov <gsshopov@gmail.com>
2016-01-10 15:31:21 -0500

    uri; solve 1332 "One-Two-Three"

--- uri/strings/README.md
+++ uri/strings/README.md
@@ -1,9 +1,12 @@
 # Strings
 
-|  ID  | Problem statement |  Solution   |
-|:----:|:------------------|:-----------:|
-| 1168 | [LED][]           | [led.cpp][] |
+|  ID  | Problem statement |       Solution        |
+|:----:|:------------------|:---------------------:|
+| 1168 | [LED][]           | [led.cpp][]           |
+| 1332 | [One-Two-Three][] | [one_two_three.cpp][] |
 
-[LED]: https://www.urionlinejudge.com.br/judge/en/problems/view/1168
+[LED]:           https://www.urionlinejudge.com.br/judge/en/problems/view/1168
+[One-Two-Three]: https://www.urionlinejudge.com.br/judge/en/problems/view/1332
 
-[led.cpp]: led.cpp
+[led.cpp]:           led.cpp
+[one_two_three.cpp]: one_two_three.cpp
---
+++ uri/strings/one_two_three.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+inline
+void use_io_optimizations()
+{
+    ios_base::sync_with_stdio();
+    cin.tie(nullptr);
+}
+
+int main()
+{
+    use_io_optimizations();
+
+    unsigned int words;
+    cin >> words;
+
+    for (unsigned int i {0}; i < words; ++i)
+    {
+        string word;
+        cin >> word;
+
+        if (word.length() == 5)
+        {
+            cout << 3 << '\n';
+        }
+        else if (word[0] == 't' && word[1] == 'w' ||
+                 word[0] == 't' && word[2] == 'o' ||
+                 word[1] == 'w' && word[2] == 'o')
+        {
+            cout << 2 << '\n';
+        }
+        else
+        {
+            cout << 1 << '\n';
+        }
+    }
+
+    return 0;
+}
