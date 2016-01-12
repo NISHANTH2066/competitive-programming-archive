2a3eeb1a70f35677461c74013aad5ab30e8007a2
Georgi Shopov <gsshopov@gmail.com>
2016-01-12 15:50:26 -0500

    uri; solve "Longtail Hedgehog" from Round #338 (Div. 2)

--- codeforces/round-338/div-2/README.md
+++ codeforces/round-338/div-2/README.md
@@ -1,5 +1,7 @@
 # Round #338 (Div. 2)
 
 * [A. Bulbs][]
+* [B. Longtail Hedgehog][]
 
-[A. Bulbs]: http://codeforces.com/contest/615/problem/A
+[A. Bulbs]:             http://codeforces.com/contest/615/problem/A
+[B. Longtail Hedgehog]: http://codeforces.com/contest/615/problem/B
---
+++ codeforces/round-338/div-2/longtail_hedgehog.cpp
@@ -0,0 +1,66 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+inline
+void use_io_optimizations()
+{
+    ios_base::sync_with_stdio();
+    cin.tie(nullptr);
+}
+
+void read_input(unsigned int& points,
+                unsigned int& segments,
+                vector<vector<unsigned int>>& graph,
+                vector<unsigned long long>& lengths)
+{
+    cin >> points >> segments;
+
+    graph.resize(points);
+    lengths.resize(points, 1);
+
+    for (unsigned int i {0}; i < segments; ++i)
+    {
+        unsigned int from;
+        unsigned int to;
+
+        cin >> from >> to;
+
+        graph[from - 1].push_back(to - 1);
+        graph[to - 1].push_back(from - 1);
+    }
+}
+
+int main()
+{
+    use_io_optimizations();
+
+    unsigned int points;
+    unsigned int segments;
+
+    vector<vector<unsigned int>> graph;
+    vector<unsigned long long> lengths;
+
+    read_input(points, segments, graph, lengths);
+
+    unsigned long long max_beauty {0};
+
+    for (unsigned int i {0}; i < points; ++i)
+    {
+        for (auto adjacent : graph[i])
+        {
+            if (adjacent < i)
+            {
+                lengths[i] = max(lengths[i], lengths[adjacent] + 1);
+            }
+        }
+
+        max_beauty = max(max_beauty, lengths[i] * graph[i].size());
+    }
+
+    cout << max_beauty << '\n';
+
+    return 0;
+}
