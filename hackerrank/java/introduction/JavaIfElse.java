24ed314708ff24abe810fe67596b6c2f2adf26f6
Georgi Shopov <gsshopov@gmail.com>
2016-03-22 19:13:29 -0400

    hackerrank; solve "Java If-Else" from java

---
+++ hackerrank/java/introduction/JavaIfElse.java
@@ -0,0 +1,62 @@
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.io.IOException;
+
+import java.util.StringTokenizer;
+
+public class JavaIfElse {
+    public static void main(String[] args) {
+        InputReader in  = new InputReader(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+
+        int number = in.nextInt();
+
+        if (number % 2 == 1) {
+            out.println("Weird");
+        } else if (number <= 5) {
+            out.println("Not Weird");
+        } else if (number <= 20) {
+            out.println("Weird");
+        } else {
+            out.println("Not Weird");
+        }
+
+        in.close();
+        out.close();
+    }
+
+    private static class InputReader {
+        private BufferedReader  reader;
+        private StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream));
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException exception) {
+                    throw new RuntimeException(exception);
+                }
+            }
+
+            return tokenizer.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public void close() {
+            try {
+                reader.close();
+            } catch (IOException exception) {
+                throw new RuntimeException(exception);
+            }
+        }
+    }
+}
--- hackerrank/java/introduction/README.md
+++ hackerrank/java/introduction/README.md
@@ -4,9 +4,12 @@
 |:----------------------------|:----------------------------:|
 | [Welcome to Java!][]        | [WelcomeToJava.java][]       |
 | [Java Stdin and Stdout 1][] | [JavaStdinAndStdout1.java][] |
+| [Java If-Else][]            | [JavaIfElse.java][]          |
 
 [Welcome to Java!]:        https://www.hackerrank.com/challenges/welcome-to-java
 [Java Stdin and Stdout 1]: https://www.hackerrank.com/challenges/java-stdin-and-stdout-1
+[Java If-Else][]:          https://www.hackerrank.com/challenges/java-if-else
 
 [WelcomeToJava.java]:       WelcomeToJava.java
 [JavaStdinAndStdout1.java]: JavaStdingAndStdout1.java
+[JavaIfElse.java]:          JavaIfElse.java
