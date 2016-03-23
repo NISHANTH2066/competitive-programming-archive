dc665f48ed0da8b9cb84cb2c635ffbf96e2877b8
Georgi Shopov <gsshopov@gmail.com>
2016-03-22 22:50:15 -0400

    hackerrank; solve "Java Output Formatting" from Java

---
+++ hackerrank/java/introduction/JavaOutputFormatting.java
@@ -0,0 +1,64 @@
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.io.IOException;
+
+import java.util.StringTokenizer;
+
+public class JavaOutputFormatting {
+    public static void main(String[] args) throws IOException {
+        InputReader in  = new InputReader(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+
+        new Problem().solve(in, out);
+
+        in.close();
+        out.close();
+    }
+
+    private static class Problem {
+        public void solve(InputReader in, PrintWriter out) throws IOException {
+            out.println("================================");
+
+            for (int i = 0; i < 3; ++i) {
+                out.printf("%-15s%03d\n", in.next(), in.nextInt());
+            }
+
+            out.println("================================");
+        }
+    }
+
+    private static class InputReader {
+        private BufferedReader  reader;
+        private StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream));
+        }
+
+        public String next() throws IOException {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                tokenizer = new StringTokenizer(reader.readLine());
+            }
+
+            return tokenizer.nextToken();
+        }
+
+        public int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+
+        public double nextDouble() throws IOException {
+            return Double.parseDouble(next());
+        }
+
+        public String nextLine() throws IOException {
+            return reader.readLine();
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+}
--- hackerrank/java/introduction/README.md
+++ hackerrank/java/introduction/README.md
@@ -1,18 +1,21 @@
 # Introduction
 
-|      Problem statement      |           Solution           |
-|:----------------------------|:----------------------------:|
-| [Welcome to Java!][]        | [WelcomeToJava.java][]       |
-| [Java Stdin and Stdout 1][] | [JavaStdinAndStdout1.java][] |
-| [Java If-Else][]            | [JavaIfElse.java][]          |
-| [Java Stdin and Stdout 2][] | [JavaStdinAndStdout2.java][] |
+|      Problem statement      |           Solution            |
+|:----------------------------|:-----------------------------:|
+| [Welcome to Java!][]        | [WelcomeToJava.java][]        |
+| [Java Stdin and Stdout 1][] | [JavaStdinAndStdout1.java][]  |
+| [Java If-Else][]            | [JavaIfElse.java][]           |
+| [Java Stdin and Stdout 2][] | [JavaStdinAndStdout2.java][]  |
+| [Java Output Formatting][]  | [JavaOutputFormatting.java][] |
 
 [Welcome to Java!]:        https://www.hackerrank.com/challenges/welcome-to-java
 [Java Stdin and Stdout 1]: https://www.hackerrank.com/challenges/java-stdin-and-stdout-1
 [Java If-Else]:            https://www.hackerrank.com/challenges/java-if-else
 [Java Stdin and Stdout 2]: https://www.hackerrank.com/challenges/java-stdin-stdout
+[Java Output Formatting]:  https://www.hackerrank.com/challenges/java-output-formatting
 
-[WelcomeToJava.java]:       WelcomeToJava.java
-[JavaStdinAndStdout1.java]: JavaStdingAndStdout1.java
-[JavaIfElse.java]:          JavaIfElse.java
-[JavaStdinAndStdout2.java]: JavaStdinAndStdout2.java
+[WelcomeToJava.java]:        WelcomeToJava.java
+[JavaStdinAndStdout1.java]:  JavaStdingAndStdout1.java
+[JavaIfElse.java]:           JavaIfElse.java
+[JavaStdinAndStdout2.java]:  JavaStdinAndStdout2.java
+[JavaOutputFormatting.java]: JavaOutputFormatting.java
