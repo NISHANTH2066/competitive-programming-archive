acdc8ca6c367547d5127206949175504bda0d189
Georgi Shopov <gsshopov@gmail.com>
2016-03-23 12:07:04 -0400

    hackerrank; solve "Java Strings Introduction" from Java

---
+++ hackerrank/java/strings/JavaStringsIntroduction.java
@@ -0,0 +1,77 @@
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.io.IOException;
+
+import java.util.StringTokenizer;
+
+import java.math.BigInteger;
+
+public class JavaStringsIntroduction {
+    public static void main(String[] args) throws IOException {
+        InputReader in  = new InputReader(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+
+        new Problem().solve(in, out);
+
+        in.close();
+        out.close();
+    }
+
+    private static class Problem {
+        public void solve(InputReader in, PrintWriter out) throws IOException {
+            String first  = in.next();
+            String second = in.next();
+
+            out.println(first.length() + second.length());
+            out.println(first.compareTo(second) > 0 ? "Yes" : "No");
+            out.println(capitalize(first) + " " + capitalize(second));
+        }
+
+        private String capitalize(String string) {
+            return Character.toUpperCase(string.charAt(0)) + string.substring(1);
+        }
+    }
+
+    private static class InputReader {
+        private BufferedReader  reader;
+        private StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream));
+        }
+
+        public String next() throws IOException {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                tokenizer = new StringTokenizer(reader.readLine());
+            }
+
+            return tokenizer.nextToken();
+        }
+
+        public int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() throws IOException {
+            return Long.parseLong(next());
+        }
+
+        public BigInteger nextBigInteger() throws IOException {
+            return new BigInteger(next());
+        }
+
+        public double nextDouble() throws IOException {
+            return Double.parseDouble(next());
+        }
+
+        public String nextLine() throws IOException {
+            return reader.readLine();
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+}
---
+++ hackerrank/java/strings/README.md
@@ -0,0 +1,9 @@
+# Strings
+
+|       Problem statement       |             Solution             |
+|:------------------------------|:--------------------------------:|
+| [Java Strings Introduction][] | [JavaStringsIntroduction.java][] |
+
+[Java Strings Introduction]: https://www.hackerrank.com/challenges/java-strings-introduction
+
+[JavaStringsIntroduction.java]: JavaStringsIntroduction.java
