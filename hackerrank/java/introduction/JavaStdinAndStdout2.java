bfc62d7544667220ad7229c1fc35e09e3e0ccdd9
Georgi Shopov <gsshopov@gmail.com>
2016-03-22 20:27:12 -0400

    hackerrank; solve "Java Stdin and Stdout 2" from Java

---
+++ hackerrank/java/introduction/JavaStdinAndStdout2.java
@@ -0,0 +1,58 @@
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.io.IOException;
+
+import java.util.StringTokenizer;
+
+public class JavaStdinAndStdout2 {
+    public static void main(String[] args) throws IOException {
+        InputReader in  = new InputReader(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+
+        int integer = in.nextInt();
+        double real = in.nextDouble();
+        String line = in.nextLine();
+
+        out.println("String: " + line);
+        out.println("Double: " + real);
+        out.println("Int: " + integer);
+
+        in.close();
+        out.close();
+    }
+
+    private static class InputReader {
+        private BufferedReader  reader;
+        private StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream));
+        }
+
+        public String next() throws IOException {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                tokenizer = new StringTokenizer(reader.readLine());
+            }
+
+            return tokenizer.nextToken();
+        }
+
+        public int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+
+        public double nextDouble() throws IOException {
+            return Double.parseDouble(next());
+        }
+
+        public String nextLine() throws IOException {
+            return reader.readLine();
+        }
+
+        public void close() throws IOException {
+            reader.close();
+        }
+    }
+}
--- hackerrank/java/introduction/README.md
+++ hackerrank/java/introduction/README.md
@@ -5,11 +5,14 @@
 | [Welcome to Java!][]        | [WelcomeToJava.java][]       |
 | [Java Stdin and Stdout 1][] | [JavaStdinAndStdout1.java][] |
 | [Java If-Else][]            | [JavaIfElse.java][]          |
+| [Java Stdin and Stdout 2][] | [JavaStdinAndStdout2.java][] |
 
 [Welcome to Java!]:        https://www.hackerrank.com/challenges/welcome-to-java
 [Java Stdin and Stdout 1]: https://www.hackerrank.com/challenges/java-stdin-and-stdout-1
-[Java If-Else][]:          https://www.hackerrank.com/challenges/java-if-else
+[Java If-Else]:            https://www.hackerrank.com/challenges/java-if-else
+[Java Stdin and Stdout 2]: https://www.hackerrank.com/challenges/java-stdin-stdout
 
 [WelcomeToJava.java]:       WelcomeToJava.java
 [JavaStdinAndStdout1.java]: JavaStdingAndStdout1.java
 [JavaIfElse.java]:          JavaIfElse.java
+[JavaStdinAndStdout2.java]: JavaStdinAndStdout2.java
