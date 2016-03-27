36c6157094f11e5ef0848a3becffb809441821d5
Georgi Shopov <gsshopov@gmail.com>
2016-03-26 20:19:46 -0400

    usaco; simplify "Friday the Thirteenth" from Section 1.1

--- usaco/chapter-01/section-01/friday_the_thirteenth.cpp
+++ usaco/chapter-01/section-01/friday_the_thirteenth.cpp
@@ -5,70 +5,29 @@
 */
 
 #include <fstream>
-#include <map>
 #include <vector>
 
 using namespace std;
 
-class Weekday
+enum Weekday
 {
-public:
-    static const Weekday Monday;
-    static const Weekday Tuesday;
-    static const Weekday Wednesday;
-    static const Weekday Thursday;
-    static const Weekday Friday;
-    static const Weekday Saturday;
-    static const Weekday Sunday;
-
-    static const unsigned int days_in_week;
-
-    bool operator<(const Weekday& other) const
-    {
-        return day < other.day;
-    }
-
-    Weekday& operator+=(unsigned int days)
-    {
-        day = (day + days) % days_in_week;
-
-        return *this;
-    }
-
-private:
-    unsigned int day;
-
-    Weekday(unsigned int day):
-        day(day)
-    { }
+    Saturday, Sunday, Monday, Tuesday, Wednesday, Thursday, Friday
 };
 
-const Weekday Weekday::Monday    {0};
-const Weekday Weekday::Tuesday   {1};
-const Weekday Weekday::Wednesday {2};
-const Weekday Weekday::Thursday  {3};
-const Weekday Weekday::Friday    {4};
-const Weekday Weekday::Saturday  {5};
-const Weekday Weekday::Sunday    {6};
-
-const unsigned int Weekday::days_in_week {7};
-
-const     Weekday      start_weekday {Weekday::Saturday};
-constexpr unsigned int start_month   {1};
-constexpr unsigned int start_year    {1900};
-
-inline bool is_leap(unsigned int year)
+inline
+bool is_leap(unsigned int year)
 {
     return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
 }
 
-inline unsigned int month_days(unsigned int month, unsigned int year)
+inline
+unsigned int days_in_month(unsigned int month, unsigned int year)
 {
     static const vector<unsigned int> days {
         31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
     };
 
-    return days[month - 1] + (month == 2 ? is_leap(year) : 0);
+    return days[month - 1] + (month == 2 && is_leap(year));
 }
 
 int main()
@@ -79,25 +38,22 @@ int main()
     unsigned int period;
     fin >> period;
 
-    Weekday current_day {start_weekday};
-    map<Weekday, unsigned int> frequencies;
+    unsigned int day {Saturday};
+    vector<unsigned int> frequencies(7);
 
-    for (unsigned int year {start_year}; year < start_year + period; ++year)
+    for (unsigned int year {1900}; year < 1900 + period; ++year)
     {
-        for (unsigned int month {start_month}; month <= 12; ++month)
+        for (unsigned int month {1}; month <= 12; ++month)
         {
-            ++frequencies[current_day];
-            current_day += month_days(month, year);
+            ++frequencies[day];
+            day = (day + days_in_month(month, year)) % 7;
         }
     }
 
-    fout << frequencies[Weekday::Saturday]  << ' '
-         << frequencies[Weekday::Sunday]    << ' '
-         << frequencies[Weekday::Monday]    << ' '
-         << frequencies[Weekday::Tuesday]   << ' '
-         << frequencies[Weekday::Wednesday] << ' '
-         << frequencies[Weekday::Thursday]  << ' '
-         << frequencies[Weekday::Friday]    << '\n';
+    for (auto i = frequencies.cbegin(); i != frequencies.cend(); ++i)
+    {
+        fout << *i << " \n"[i + 1 == frequencies.cend()];
+    }
 
     return 0;
 }
